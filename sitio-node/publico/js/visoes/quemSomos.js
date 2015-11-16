7f98f8db343e3785507b5dedb3c1ffcb9f3c8ec7
umd <leofelippedias@facebook.com>
2015-11-16 19:14:32 -0200

    Adicionado mais funções para controlar a navegação. Estamos utilizando mais do scrollTo e eventos. Removido muito do scrollspy

--- sitio-node/publico/js/visoes/quemSomos.js
+++ sitio-node/publico/js/visoes/quemSomos.js
@@ -7,21 +7,44 @@ window.VisaoQuemSomos = Backbone.View.extend({
   },
 
   render: function () {
-    // Renderiza este template
+    // Carrega e retorna o conteúdo da visão que será inserido.
     $(this.el).html(this.template());
     return this;
   },
   
   /* Aqui selecionamos um item do menu vertical */
-  selecionarItemNavegacao: function(item) {
+  iniciarEventosParaMenu: function(item) {
+    
+    // Adicionamos o seletor e também o offset.
+    $('div#' + 'textoQuemSomos').scrollspy({
+      selector: 'a#rolagemId',
+      offset: 30
+    });
     
     // Remove seleção atual
     $('#menuVertEsquerdo .nav li').removeClass('active');
     
-    // Seleciona item da barra de navegação vertical
-    if (item) {
-      $('#' + item).addClass('active');
-    }
+    // Aqui nós adicionamos a seleção ao item inicial.
+    $('li#' + item).addClass('active');
+    
+    // Adicionamos o evento de clique nos links
+    $('a#rolagemId').click(function(evento){
+      // Fazemos com que o link não prossiga para barra de endereços.
+      evento.preventDefault();
+      
+      // Remove seleção atual
+      $('#menuVertEsquerdo .nav li').removeClass('active');
+    
+      // Pegamos o elemento que miramos.
+      var mira = $(this).attr('mira');
+      
+      $('div#' + 'textoQuemSomos').scrollTo(
+        mira, 
+        {duration: 500, offset: -30}
+      );
+      
+    });
+    
   }
   
 });
