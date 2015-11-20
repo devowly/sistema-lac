94c35f0217e41b0e7624594a0e5515afd9f44985
umd <leofelippedias@facebook.com>
2015-11-20 19:38:31 -0200

    Adicionado associação entre coordenadas e unidades. adc mais comentários

--- sitio-node/fonte/armazenamento/modelos/Unidade.js
+++ sitio-node/fonte/armazenamento/modelos/Unidade.js
@@ -2,22 +2,19 @@
 
 /* @arquivo Unidade.js */
 
-// @AFAZER: Adicionar o tamanho máximo de cada item 
-// @AFAZER: Adicionar associação com as coordenadas.
+// ATENÇÃO: Por enquanto não vamos utilizar isso porque é mais produtivo definir as 
+// Coordenadas de Unidades diretamente em arquivo html.
+
 module.exports = function (database, DataTypes) {
 
   var Unidade = database.define('Unidade', {
     nome: {
-      type: DataTypes.STRING, // <umdez> Qual será o tamanho desta coluna?
-      validate: {} // <umdez> Como realizar esta validação?
-    },
-    texto: {
-      type: DataTypes.STRING, // <umdez> Qual será o tamanho desta coluna?
-      validate: {} // <umdez> Como realizar esta validação?
+      type: DataTypes.STRING, // Por padrão o tamanho é de 255 caracteres.
+      validate: {} 
     }
   }, {
     associate: function (modelos) {
-
+      
     },
     instanceMethods: {
     
--- sitio-node/fonte/armazenamento/modelos/UnidadeCoordenada.js
+++ sitio-node/fonte/armazenamento/modelos/UnidadeCoordenada.js
@@ -2,20 +2,28 @@
 
 /* @arquivo UnidadeCoordenada.js */
 
+// ATENÇÃO: Por enquanto não vamos utilizar isso porque é mais produtivo definir as 
+// Coordenadas de Unidades diretamente em arquivo html.
+
 module.exports = function (database, DataTypes) {
 
   var UnidadeCoordenada = database.define('UnidadeCoordenada', {
     latitude: {
-      type: DataTypes.STRING,
+      type: DataTypes.CHAR,
       validate: {}
     },
     longitude: {
-      type: DataTypes.STRING,
+      type: DataTypes.CHAR,
       validate: {}
     }
   }, {
     associate: function (modelos) {
-
+      
+      // Adiciona coluna de chave estrangeira 'UnidadeId' para esta tabela.
+      modelos.UnidadeCoordenada.belongsTo(modelos.Unidade, {
+        
+      }); 
+      
     },
     instanceMethods: {
     
