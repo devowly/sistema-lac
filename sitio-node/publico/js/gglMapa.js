7b5b1b7bb37a36a911795bc9b5693de7477274e8
umd <leofelippedias@facebook.com>
2015-11-22 17:58:53 -0200

    adc. novos métodos

--- sitio-node/publico/js/gglMapa.js
+++ sitio-node/publico/js/gglMapa.js
@@ -3,8 +3,13 @@
 /* Objeto para lidar com o mapa do google que será utilizado na página de unidades */
 window.gglMapa = {
   
+  // Versão da API para o mapa.
+  GMAPA_VERSAO: 3,
+  
+  seMapaApiPronto: false, 
+  
   /* função chamada logo após o mapa for carregado. Ela é chamada porque adicionamos como uma propriedade no endereço fonte.
-   * @Veja https://maps.googleapis.com/maps/api/js?key=AIzaSyCbi1li8ie5oMqH5pjgok6Y0Xb-NUrW3ag&callback=gglMapa.carregouMapaApi */
+   * @Veja https://maps.googleapis.com/maps/api/js?key=API-KEY&callback=gglMapa.carregouMapaApi */
   carregouMapaApi: function () {
     this.seMapaApiPronto = true;
   },
@@ -12,6 +17,40 @@ window.gglMapa = {
   /* Função que retorna se a API do Google Mapas está carregada. */
   seMapaPronto: function() {
     return this.seMapaApiPronto;
-  }
+  },
+
+  centralizarMapa: function (coordenadas, zoom, elemento) {
+    var mapa;
     
+    // Centraliza e faz um zoom 
+    mapa = new google.maps.Map( elemento , {
+      center: coordenadas,
+      zoom: zoom
+    });
+    
+    return mapa;
+  },
+  
+  adcrMarcadorMapa: function (mapa, coordenadas, titulo) {
+  
+    // Coloca uma marca de unidade no mapa na posição das coordenadas informadas.
+    var marca = new google.maps.Marker({
+      position: coordenadas,
+      map: mapa,
+      title: titulo
+    }); 
+    
+    return marca;
+  },
+  
+  redimensionarMapa: function(mapa) {
+    
+    if (this.GMAPA_VERSAO === 3) {
+      google.maps.event.trigger(mapa, 'resize');
+    } else if (this.GMAPA_VERSAO === 2) {
+      mapa.checkResize();
+    } else {
+      console.log('Versão do mapa não disponível.');
+    }
+  }
 }
