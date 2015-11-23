936a126d498fc110b15a6f05c7f46330dd589c89
umd <leofelippedias@facebook.com>
2015-11-23 13:14:40 -0200

    editado nome da função de carregar templantes e adicionado mais comentarios

--- sitio-node/publico/js/utilitarios.js
+++ sitio-node/publico/js/utilitarios.js
@@ -22,10 +22,12 @@ window.utilitarios = {
   },
   
   /* Carrega assincronamente o(s) template(s) encontrado(s) em arquivos .html separados do diretorio informado.
-   * Ele armazena em cada objeto da lista um template.
-   * Depois de carregado ele chama a função cd().
+   * Ele armazena em cada objeto da lista um template. Depois de carregado ele chama a função cd().
+   * Este método é designado para uso daquelas visões que precisam carregar mais arquivos quando não utiliza o banco de dados.
+   * É interessante usa-lo pois isso faz o desenvolvimento ser mais produtivo porque não precisamos adicionar modelos e coleções e 
+   * também faz com que não seja necessário carregar dados para o banco de dados.
    */
-  carregarTemplantesVisao: function(lista, diretorio, visoes, cd) {
+  carregarTemplantesDinamicamente: function(lista, diretorio, visoes, cd) {
 
     var deferidos = [];
 
