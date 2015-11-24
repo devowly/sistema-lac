966a33e97cf2c882b15b8fea0e222b3f612842d7
umd <leofelippedias@facebook.com>
2015-11-24 10:47:02 -0200

    editado

---
+++ sitio-node/fonte/servicoRest/modelos/UnidadeCoordenada.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = {
+  nome: 'UnidadeCoordenada', // Isso deve ser o mesmo nome dado a tabela no banco de dados.
+  rotas: ['/coordenadas', '/coordenadas/:id'] // Rotas para o serviço REST.
+}
