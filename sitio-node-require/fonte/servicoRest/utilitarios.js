1f276f7e8178853406657f0edb5279edc1abdf21
umd <leofelippedias@facebook.com>
2015-12-23 15:37:22 -0200

    edi.t

--- sitio-node-require/fonte/servicoRest/utilitarios.js
+++ sitio-node-require/fonte/servicoRest/utilitarios.js
@@ -1,144 +1,105 @@
 'use strict'
+var deasync = require('deasync');
+var Bandeiras = require('./Bandeiras');
+var AutenticacaoUsuario = require('./AutenticacaoUsuario');
 
 /* Utilitarios diversos para nossas fontes REST.
  *
- * @Arquivo utilitarios.js
+ * @Arquivo Utilitarios.js
  */
  
-var utilitarios = {
-  bd: null,
-  jsonWebToken: null
-};
-
 /* Utilitários diversos para as fontes Rest.
  *
  * @Parametro {bancoDados} Objeto do nosso banco de dados.
  * @Parametro {jwt} Nosso módulo Json Web Token.
- * @Parametro {superSecreto} Segredo utilizado ao codificar e decodificar os tokens.
+ * @Parametro {autenticacao} Dados para autenticação.
  */
-utilitarios.inicializar = function(bancoDados, jwt, superSecreto) {
-  // Acesso ao banco de dados.
-  this.bd = bancoDados;
+var Utilitarios = function (bancoDados, jwt, autenticacao) {
+
+  // Armazena a classe do banco de dados sequelize.
+  this.bd = bancoDados; 
   
-  // Acesso ao nosso método Json Web Token de autorização as nossas rotas.
+  // Utilizaremos os tokens para autenticação.
   this.jsonWebToken = jwt;
   
-  // O valor super secreto para a codificação e decodificação dos tokens.
-  this.superSecreto = superSecreto;
-};
-
-/* Realiza verificação do nosso usuário pelo token. 
- * @Veja https://github.com/auth0/node-jsonwebtoken
- * @Veja http://brianmajewski.com/2015/02/25/relearning-backbone-part-9/
- *
- * @Parametro {token} O token informado pelo usuário.
- * @Parametro {modeloAcessoRota} O nome do modelo que contem as bandeiras de acesso para as rotas.
- * @Parametro {modeloRota} O nome do modelo onde as rotas estão sendo acessadas.
- * @Parametro {cd} Função chamada logo após verificarmos completamente o usuário.
- */
-utilitarios.verificarUsuarioToken = function(token, modeloAcessoRota, modeloRota, cd) {
-  var esteObjeto = this;
+  // Configuração da autenticação.
+  this.autentic = autenticacao;
   
-  this.jsonWebToken.verify(token, this.superSecreto, function (erro, decodificado) {
-    if (erro) {
-      cd(false, null);
-    } else {
-      
-      if (decodificado) {
-        var usuarioAcesso = {};
-        // Aqui nós iremos procurar pelas bandeiras que este usuário possui para a rota (modelo) informada.
-        esteObjeto.bd[modeloAcessoRota].findOne({
-          where: {
-            usuario_id: decodificado.id,  // Identificador do usuário.
-            modelo: modeloRota            // Modelo onde queremos descobrir as bandeiras de acesso.
-          }
-        }).then(function (acessoRota) {
-          if (!acessoRota) {
-            // Aqui, caso o usuário não possua nenhuma bandeira para este modelo, retornamos o valor de false. Isso
-            // faz com que o usuário não tenha acesso as rotas que necessitem de uma bandeira de acesso.
-            // As rotas de livre acesso não necessitam de nenhuma verificação.
-            cd(false, null);
-          } else {
-            // Copiamos alguns dados necessário para usuarioAcesso e depois iremos retorna-los.
-            usuarioAcesso.id = decodificado.id;       // id: Identificador e também chave primária do nosso usuário.
-            usuarioAcesso.jid = decodificado.jid;     // jid: Identificador Jabber do nosso usuário. (local@dominio).
-            usuarioAcesso.uuid = decodificado.uuid;   // uuid: Identificador único do usuário.
-            usuarioAcesso.name = decodificado.name;   // name: Nome do usuário.
-            usuarioAcesso.bandeira = parseInt(acessoRota.bandeira, 16);  // bandeira: A bandeira de acesso deste usuário.
-            cd(true, usuarioAcesso);
-          }
-        });
-      } else {
-        cd(false, null);
-      }
-    }
-  });
+  this.autenticacaoUsuario = new AutenticacaoUsuario(bancoDados, jwt, autenticacao); 
+  
+  this.bandeiras = new Bandeiras(); 
 };
 
-/* Verificamos o usuário. A verificação é realizada comparando a senha com a senha que temos.
- * @Veja http://brianmajewski.com/2015/02/25/relearning-backbone-part-9/
+Utilitarios.prototype.inicializar = function () {
+  
+};
+
+/* Realiza a autenticação de deteminado usuário pelo token informado.
  *
- * @Parametro {modeloVerificacao} O nome do modelo em que iremos verificar o nosso usuário.
- * @Parametro {modeloAcessoRota} O nome do modelo que contem as bandeiras de acesso para as rotas.
- * @Parametro {modeloRota} O nome do modelo onde as rotas estão sendo acessadas.
- * @Parametro {usuarioJid} O identificador do usuário. Composto de local@dominio.
- * @Parametro {senha} Senha do usuário.
- * @Parametro {cd} Função chamada logo após verificarmos completamente o usuário.
+ * @Parametro {token} Aquele token utilizado para autenticação.
+ * @Parametro {modeloRota} Cada modelo possuirá bandeiras de acesso e nós iremos acessar o valor das bandeiras de determinado modelo.
+ * @Parametro {cd} A função chamada após a autenticação.
  */
-utilitarios.verificarUsuario = function(modeloVerificacao, modeloAcessoRota, modeloRota, usuarioJid, senha, cd) {
-  // Aqui procuramos o usuário pelo jid fornecido.
-  this.bd[modeloVerificacao].findOne({
-    where: {
-      jid: usuarioJid
-    }
-  }).then(function (usuario) {
-    // Se não houver um usuário, é provavel que os dados informados estejam incorretos. Retornamos assim o valor false.
-    if (!usuario) {
-      cd(false, null);
-    } else {
-      // Iremos verificar aqui se os dados informados realmente conferem com os dados que temos.
-      var seConfere = usuario.verificarSenha(senha);
-      if (seConfere) {
-        // Se o usuário conferir com os dados, então agora queremos acessar sua bandeira de acesso a esta rota.
-        utilitarios.verificarUsuarioAcessoRota(modeloRota, modeloAcessoRota, usuario, cd);
-      } else {
-        cd(false, null);
-      }
-    }
+Utilitarios.prototype.autenticarPeloToken = function (token, modeloRota, cd) {
+  var seTerminou = false;  // Informa quando validação estiver terminada.
+  var dadosUsuario = null; // Dados do usuário.
+  var seValidado = false;  // Informamos se o usuário foi validado.
+  
+  var ponte = function(seConfere, usuario) {
+    if (seConfere) {
+      // Nosso usuário foi validado com sucesso.
+      dadosUsuario = usuario;
+      seValidado = seConfere;
+    } 
+    // Quando realizado nossa verificação então continuamos a execução.
+    seTerminou = true;
+  }
+  
+  // Verificamos o token.
+  this.autenticacaoUsuario.verificarUsuarioPeloToken(token, modeloRota, ponte);
+  
+  // Percorre laço enquanto não estiver realizado tudo. Infelizmente, 
+  // isso é necessário porque o sequelize é assincrono.
+  deasync.loopWhile(function(){
+    return !seTerminou;
   });
+  
+  cd(seValidado, dadosUsuario);
 };
 
-/* Logo após o usuário for verificado, nós carregamos as bandeiras de acesso dele a determinada rota.
+/* Realiza a autenticação do usuário pelo JID. Se o usuário conferir, vamos retornar suas informações com a função cd(), 
+ * juntamente com o valor da sua bandeira de acesso a um determinado modelo.
  *
- * @Parametro {modeloRota} O nome do modelo onde as rotas estão sendo acessadas.
- * @Parametro {modeloAcessoRota} O nome do modelo que contem as bandeiras de acesso para as rotas.
- * @Parametro {usuario} O objeto com os dados do usuário. 
- * @Parametro {cd} Função chamada logo após acessarmos as bandeiras de determinado usuário.
+ * @Parametro {modeloRota} O modelo onde iremos pegar as bandeiras de acesso do usuário.
+ * @Parametro {usrjid} O identificador do usuário. Composto de local@dominio.
+ * @Parametro {senha} A senha deste usuário.
+ * @Parametro {cd} Função que será chamada assim que a verificação estiver terminada.
  */
-utilitarios.verificarUsuarioAcessoRota = function(modeloRota, modeloAcessoRota, usuario, cd) {
-  var usuarioAcesso = {};
-  // Aqui nós iremos procurar pelas bandeiras que este usuário possui para a rota (modelo) informada.
-  this.bd[modeloAcessoRota].findOne({
-    where: {
-      usuario_id: usuario.id,       // Identificador do usuário.
-      modelo: modeloRota            // Modelo onde queremos descobrir as bandeiras de acesso.
-    }
-  }).then(function (acessoRota) {
-    if (!acessoRota) {
-      // Aqui, caso o usuário não possua nenhuma bandeira para este modelo, retornamos o valor de false. Isso
-      // faz com que o usuário não tenha acesso as rotas que necessitem de uma bandeira de acesso.
-      // As rotas de livre acesso não necessitam de nenhuma verificação.
-      cd(false, null);
-    } else {
-      // Copiamos alguns dados necessário para usuarioAcesso e depois iremos retorna-los.
-      usuarioAcesso.id = usuario.id;       // id: Identificador e também chave primária do nosso usuário.
-      usuarioAcesso.jid = usuario.jid;     // jid: Identificador Jabber do nosso usuário. (local@dominio).
-      usuarioAcesso.uuid = usuario.uuid;   // uuid: Identificador único do usuário.
-      usuarioAcesso.name = usuario.name;   // name: Nome do usuário.
-      usuarioAcesso.bandeira = parseInt(acessoRota.bandeira, 16);  // bandeira: A bandeira de acesso deste usuário.
-      cd(true, usuarioAcesso);
-    }
+Utilitarios.prototype.autenticarPeloJid = function(modeloRota, usrjid, senha, cd) { 
+  var seTerminou = false;  // Informa quando validação estiver terminada.
+  var dadosUsuario = null; // Dados do usuário.
+  var seValidado = false;  // Informamos se o usuário foi validado.
+  
+  var ponte = function(seConfere, usuario) {
+    if (seConfere) {
+      // Nosso usuário foi validado com sucesso.
+      dadosUsuario = usuario;
+      seValidado = seConfere;
+    } 
+    // Quando realizado nossa verificação então continuamos a execução.
+    seTerminou = true;
+  }
+
+  // Verificamos jid e senha.
+  this.verificarUsuarioPeloJid(modeloRota, usrjid, senha, ponte);
+  
+  // Percorre laço enquanto não estiver realizado tudo. Infelizmente, 
+  // isso é necessário porque o sequelize é assincrono.
+  deasync.loopWhile(function(){
+    return !seTerminou;
   });
+  
+  cd(seValidado, dadosUsuario);
 };
 
-module.exports = utilitarios;
+module.exports = Utilitarios;
