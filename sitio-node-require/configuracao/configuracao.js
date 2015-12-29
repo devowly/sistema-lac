837b2ad87c1a78ce8642d25db3518bc716d36c75
umd <leofelippedias@facebook.com>
2015-12-29 13:40:02 -0200

    Adicionar dados de certificados e sessão para o arquivo de configuração.

--- sitio-node-require/configuracao/configuracao.js
+++ sitio-node-require/configuracao/configuracao.js
@@ -14,13 +14,17 @@
  *  - storage.user (Obrigatório) O nome do usuário do banco. Não necessário para o SQLite.
  *
  * @Diretiva {server} O nosso servidor http.
- *  - server.port (Opcional e recomendado) A porta onde o serviço irá escutar.
+ *  - server.port (Obrigatório) A porta onde o serviço irá esperar por requisições http.
+ *  - server.sslPort (Obrigatório) A porta ao qual o servidor irá esperar por requisições https.
  *  - server.limit (Obrigatório) Valor limite do body que é permitido. Mantenha o valor baixo para precaver
  *                               contra negação de serviços.
  *  - server.cors (Opcional) Se iremos oferecer o serviço cors.
  *  - server.cors.origin (Obrigatório) O endereço de origem que é permitidos pelo cors. Por questões de segurança, 
  *                                     utilize * apenas para a fase de desenvolvimento e testes.
  *  - server.logger (Opcional) O tipo de registro. podendo ser: 'default', 'short', 'tiny', 'dev' 
+ *  - server.session.superSecret (Obrigatório) Super segredo utilizado pela sessão.
+ *  - server.certificates.privateKey (Obrigatório) A chave privada.
+ *  - server.certificates.certificate (Obrigatório) O certificado.
  *
  * @Diretiva {authentication} O nosso sistema de autenticação.
  *  - authentication.superSecret (Obrigatório) Super segredo para codificar e decoficar os tokens.
@@ -46,11 +50,19 @@ module.exports = {
   "server": {
     "logger": "dev",                  // Valores permitidos: 'default', 'short', 'tiny', 'dev' 
     "port": 80,                       // A porta ao qual o servidor irá escutar por requisições http.
+    "sslPort": 443,                   // A porta ao qual o servidor irá esperar por requisições https.
     "limit": "200kb",                 // Limite permitido para o conteúdo body. Lembre-se de manter o limit do body em 
                                       // '200kb' para nos precaver dos ataques de negação de serviço.
     "cors": {                         
       "origin": ["http://localhost"]  // O endereço de origem que é permitido, utilize * apenas na fase de desenvolvimento e testes, 
                                       // por questões de segurança.
+    },
+    "session": {
+      "superSecret": "abcd4321"       // Super segredo utilizado pela sessão.
+    },
+    "certificates": {                 // Certificados utilizados para o servidor https.
+      "privateKey": "servidorHttps.key", // A chave privada.
+      "certificate": "servidorHttps.crt" // O certificado.
     }
   },
   
