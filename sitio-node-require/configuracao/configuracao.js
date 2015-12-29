aad295e4d35c7f74d4b103a0473c9f6e920df950
umd <leofelippedias@facebook.com>
2015-12-28 23:40:47 -0200

    adc. comentários

--- sitio-node-require/configuracao/configuracao.js
+++ sitio-node-require/configuracao/configuracao.js
@@ -18,7 +18,7 @@
  *  - server.limit (Obrigatório) Valor limite do body que é permitido. Mantenha o valor baixo para precaver
  *                               contra negação de serviços.
  *  - server.cors (Opcional) Se iremos oferecer o serviço cors.
- *  - server.cors.origin (Obrigatório) Endereços de origem que são permitidos pelo cors. Por questões de segurança, 
+ *  - server.cors.origin (Obrigatório) O endereço de origem que é permitidos pelo cors. Por questões de segurança, 
  *                                     utilize * apenas para a fase de desenvolvimento e testes.
  *  - server.logger (Opcional) O tipo de registro. podendo ser: 'default', 'short', 'tiny', 'dev' 
  *
@@ -45,11 +45,11 @@ module.exports = {
   // Servidor: As configurações para o Express.
   "server": {
     "logger": "dev",                  // Valores permitidos: 'default', 'short', 'tiny', 'dev' 
-    "port": 8080,                     // A porta ao qual o servidor irá escutar por requisições http.
+    "port": 80,                       // A porta ao qual o servidor irá escutar por requisições http.
     "limit": "200kb",                 // Limite permitido para o conteúdo body. Lembre-se de manter o limit do body em 
                                       // '200kb' para nos precaver dos ataques de negação de serviço.
     "cors": {                         
-      "origin": ["http://localhost"]  // Os endereços de origem que são permitidos, utilize * apenas na fase de desenvolvimento e testes, 
+      "origin": ["http://localhost"]  // O endereço de origem que é permitido, utilize * apenas na fase de desenvolvimento e testes, 
                                       // por questões de segurança.
     }
   },
