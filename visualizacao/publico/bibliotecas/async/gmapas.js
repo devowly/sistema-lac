8df53ff72a851d782f5a0125f4d83a9dc9a7abbe
umd <leofelippedias@facebook.com>
2016-01-05 13:23:09 -0200

    removido bibliotecas

--- visualizacao/publico/bibliotecas/async/gmapas.js
+++
@@ -1,6 +0,0 @@
-// Convertemos o google mapa em AMD.
-define('gmapas', ['async!https://maps.google.com/maps/api/js?v=3&key=AIzaSyCbi1li8ie5oMqH5pjgok6Y0Xb-NUrW3ag&sensor=false'],
-function(){
-  // Retornamos o namespace do google mapas para facilitar.
-  return window.google.maps;
-});
